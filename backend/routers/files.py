82ff66853531fb9c44f2c6c667c3427325e5f17e
Albert Giegerich <a.giegerich.v@gmail.com>
2026-05-23 23:11:34 -0500

    Add get function to file_store. Use a temp dir locally to make copies of files, simulating S3 download

--- backend/routers/files.py
+++ backend/routers/files.py
@@ -1,7 +1,8 @@
+from backend.services.file_storage import get_file_storage
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.util.typing import Annotated
 from backend.database import get_session
-from backend.services.file_storage import FileStorage, LocalFileStorage, S3FileStorage
+from backend.services.file_storage import FileStore, LocalFileStore, S3FileStore
 from fastapi import Depends
 from backend.models import FileRecord
 from backend.config import settings
@@ -9,6 +10,7 @@ from fastapi import HTTPException
 from fastapi import UploadFile
 from fastapi import APIRouter
 import uuid
+from uuid import UUID
 
 router = APIRouter(
     prefix="/files",
@@ -19,25 +21,28 @@ router = APIRouter(
 DbSession = Annotated[AsyncSession, Depends(get_session)]
 
 
-def get_file_storage() -> FileStorage:
-    if settings.environment == "local":
-        return LocalFileStorage()
-
-    return S3FileStorage()
+FileStoreDep = Annotated[FileStore, Depends(get_file_storage)]
 
 
-FileStorageDep = Annotated[FileStorage, Depends(get_file_storage)]
+@router.get("/{id}")
+async def get(id: UUID, session: DbSession, file_store: FileStoreDep):
+    file_record = await session.get(FileRecord, id)
+
+    if file_record is None:
+        raise HTTPException(status_code=404, detail="File not found")
+
+    file_path = await file_store.get(file_record.path)
 
 
 @router.post("/")
-async def upload(file: UploadFile, session: DbSession, storage: FileStorageDep):
+async def upload(file: UploadFile, session: DbSession, file_store: FileStoreDep):
     if not file.filename:
         raise HTTPException(status_code=400, detail="No filename provided")
 
     id = uuid.uuid4()
     path = f"{id}/{id}"
 
-    await storage.save(file, path)
+    await file_store.save(file, path)
 
     file_record = FileRecord(id=id, path=path, name=file.filename)
     session.add(file_record)
--- backend/services/file_storage.py
+++ backend/services/file_storage.py
@@ -1,23 +1,48 @@
+import shutil
+from typing import Iterator
+from typing import Generator
 from backend.config import settings
 from fastapi import UploadFile
 from typing import Protocol
 import os
+import tempfile
 
 
-class FileStorage(Protocol):
+def get_file_storage() -> Iterator[FileStore]:
+    if settings.environment == "local":
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            yield LocalFileStore(tmp_dir)
+    else:
+        yield S3FileStore()
+
+
+class FileStore(Protocol):
     async def save(self, file: UploadFile, path: str): ...
+    async def get(self, path: str) -> str: ...
 
 
-class LocalFileStorage(FileStorage):
+class LocalFileStore(FileStore):
+    def __init__(self, tmp_dir: str):
+        self.tmp_dir = tmp_dir
+
     async def save(self, file: UploadFile, path: str):
-
         dest = os.path.join(settings.data_root, path)
         os.makedirs(os.path.dirname(dest), exist_ok=True)
 
         with open(dest, "xb") as f:
             f.write(await file.read())
 
+    async def get(self, path: str) -> str:
+        src = os.path.join(settings.data_root, path)
+        dest = os.path.join(self.tmp_dir, os.path.basename(path))
+        shutil.copy(src, dest)
 
-class S3FileStorage(FileStorage):
+        return dest
+
+
+class S3FileStore(FileStore):
     async def save(self, file: UploadFile, path: str):
+        raise NotImplementedError
+
+    async def get(self, path: str) -> str:
         raise NotImplementedError
