93d804320ba5fe47ce8591833f350bcfefa44d87
Albert Giegerich <a.giegerich.v@gmail.com>
2026-05-28 22:47:41 -0500

    Use pytest_mock to avoid using with statements for patching

--- backend/tests/test_rasters.py
+++ backend/tests/test_rasters.py
@@ -4,9 +4,10 @@ from backend.routers.rasters import RASTERS_PREFIX
 from backend.services.geotiff_service import get_geotiff_service
 from backend.services.geotiff_service import GeoTiffService
 from unittest.mock import AsyncMock, MagicMock
+from pytest_mock import MockerFixture
 from backend.models import Raster
 from typing import Iterator
-from unittest.mock import patch, mock_open
+from unittest.mock import mock_open
 
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -58,6 +59,7 @@ def test_get_raster(
     mock_db_session: AsyncMock,
     mock_file_store: AsyncMock,
     mock_geotiff_service: MagicMock,
+    mocker: MockerFixture,
 ) -> None:
     raster_path = "raster_path"
     mock_db_session.get.return_value = Raster(path=raster_path)
@@ -67,28 +69,25 @@ def test_get_raster(
 
     raster_id = uuid.uuid4()
 
-    mock_rasterio_dataset = MagicMock()
+    mock_rasterio_dataset = mocker.MagicMock()
     mock_rasterio_dataset.crs = 4326
 
     png_bytes: bytes = b"png"
-    with (
-        patch("backend.routers.rasters.rasterio.open") as mock_rasterio_open,
-        patch("builtins.open", mock_open(read_data=png_bytes)),
-    ):
-        mock_rasterio_open.return_value.__enter__.return_value = mock_rasterio_dataset
-        response = client.get(f"{RASTERS_PREFIX}/{raster_id}")
+    mock_rasterio_open = mocker.patch(
+        "backend.routers.rasters.rasterio.open", autospec=True
+    )
+    mock_rasterio_open.return_value.__enter__.return_value = mock_rasterio_dataset
+    mocker.patch("backend.routers.rasters.open", mock_open(read_data=png_bytes))
+
+    response = client.get(f"{RASTERS_PREFIX}/{raster_id}")
 
     mock_db_session.get.assert_called_once_with(Raster, raster_id)
-
     mock_file_store.get.assert_called_once_with(raster_path)
-
     mock_rasterio_open.assert_called_once_with(original_file_path)
-
     mock_geotiff_service.reproject_to_4326.assert_not_called()
 
     args, _ = mock_geotiff_service.convert_geotiff_to_png.call_args
     assert args[0] == original_file_path
-
     assert Path(args[1]).name == "png.tif"
 
     assert response.status_code == 200
@@ -101,22 +100,21 @@ def test_get_raster_reprojects_if_not_4326(
     mock_db_session: AsyncMock,
     mock_file_store: AsyncMock,
     mock_geotiff_service: MagicMock,
+    mocker: MockerFixture,
 ) -> None:
-
     mock_db_session.get.return_value = Raster()
 
     original_file_path = "original_file_path"
     mock_file_store.get.return_value = original_file_path
 
-    mock_rasterio_dataset = MagicMock()
+    mock_rasterio_dataset = mocker.MagicMock()
     mock_rasterio_dataset.crs = 9999
 
-    with (
-        patch("backend.routers.rasters.rasterio.open") as mock_rasterio_open,
-        patch("builtins.open", mock_open(read_data=b"")),
-    ):
-        mock_rasterio_open.return_value.__enter__.return_value = mock_rasterio_dataset
-        response = client.get(f"{RASTERS_PREFIX}/{EMPTY_UUID}")
+    mock_rasterio_open = mocker.patch("backend.routers.rasters.rasterio.open", autospec=True)
+    mock_rasterio_open.return_value.__enter__.return_value = mock_rasterio_dataset
+    mocker.patch("backend.routers.rasters.open", mock_open(read_data=b""))
+
+    response = client.get(f"{RASTERS_PREFIX}/{EMPTY_UUID}")
 
     reproject_to_4326_args, _ = mock_geotiff_service.reproject_to_4326.call_args
 
@@ -133,7 +131,7 @@ def test_get_raster_reprojects_if_not_4326(
     assert response.status_code == 200
 
 
-def test_list_rasters(client: TestClient, mock_db_session: AsyncMock) -> None:
+def test_list_rasters(client: TestClient, mock_db_session: AsyncMock, mocker: MockerFixture) -> None:
     raster_id = uuid.uuid4()
     raster = Raster(
         id=raster_id,
@@ -144,7 +142,7 @@ def test_list_rasters(client: TestClient, mock_db_session: AsyncMock) -> None:
         bounding_box_right=3.0,
         bounding_box_top=4.0,
     )
-    mock_result = MagicMock()
+    mock_result = mocker.MagicMock()
     mock_result.scalars.return_value.all.return_value = [raster]
     mock_db_session.execute.return_value = mock_result
 
@@ -165,7 +163,7 @@ def test_list_rasters(client: TestClient, mock_db_session: AsyncMock) -> None:
 
 
 def test_list_rasters_reprojects_bounds_if_not_4326(
-    client: TestClient, mock_db_session: AsyncMock
+    client: TestClient, mock_db_session: AsyncMock, mocker: MockerFixture
 ) -> None:
     raster_id = uuid.uuid4()
     raster = Raster(
@@ -177,16 +175,15 @@ def test_list_rasters_reprojects_bounds_if_not_4326(
         bounding_box_right=3.0,
         bounding_box_top=4.0,
     )
-    mock_result = MagicMock()
+    mock_result = mocker.MagicMock()
     mock_result.scalars.return_value.all.return_value = [raster]
     mock_db_session.execute.return_value = mock_result
 
     transformed_bounds = (10.0, 20.0, 30.0, 40.0)
 
-    with patch(
-        "backend.routers.rasters.transform_bounds", return_value=transformed_bounds
-    ):
-        response = client.get(f"{RASTERS_PREFIX}/")
+    mocker.patch("backend.routers.rasters.transform_bounds", return_value=transformed_bounds)
+
+    response = client.get(f"{RASTERS_PREFIX}/")
 
     assert response.status_code == 200
 
