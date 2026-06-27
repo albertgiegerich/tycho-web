0131ccc33a07286d404b1e1333a4d0d14daa18a7
Albert Giegerich <a.giegerich.v@gmail.com>
2026-06-26 20:02:46 -0500

    disable tiling

--- frontend/src/components/AppOverlay.tsx
+++ frontend/src/components/AppOverlay.tsx
@@ -1,5 +1,5 @@
 import { MapboxOverlay } from "@deck.gl/mapbox";
-import { BitmapLayer, TileLayer, type DeckProps, type Layer } from "deck.gl";
+import { BitmapLayer, type DeckProps, type Layer } from "deck.gl";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useControl, useMap, type LngLat } from "react-map-gl/maplibre";
 import {
@@ -149,31 +149,32 @@ const AppOverlay = () => {
   const layers: Layer[] = useMemo(() => {
     if (selectedRaster) {
       return [
-        new TileLayer({
-          id: `tile-layer`,
-          data: `http://localhost:8000/rasters/${selectedRaster.id}/tiles/{z}/{x}/{y}.png`,
-          // bounds: selectedRaster.bounds,
-          renderSubLayers: (props) => {
-            console.log(props);
-
-            const [[west, south], [east, north]] = props.tile.boundingBox;
-
-            return new BitmapLayer(props, {
-              data: undefined,
-              image: props.data,
-              bounds: [west, south, east, north],
-              // textureParameters: {
-              //   minFilter: "linear", // Use bilinear interpolation to blend the pixels when zoomed out
-              //   magFilter: "nearest", // When zoomed in show the individual pixels without interpolation
-              // },
-            });
+        new BitmapLayer({
+          id: `geotiff-bitmap`,
+          image: rasterImageUrl,
+          bounds: selectedRaster.bounds,
+          textureParameters: {
+            minFilter: "linear", // Use bilinear interpolation to blend the pixels when zoomed out
+            magFilter: "nearest", // When zoomed in show the individual pixels without interpolation
           },
         }),
+        // new TileLayer({
+        //   id: `tile-layer`,
+        //   data: `http://localhost:8000/rasters/${selectedRaster.id}/tiles/{z}/{x}/{y}.png`,
+        //   renderSubLayers: (props) => {
+        //     const [[west, south], [east, north]] = props.tile.boundingBox;
+        //     return new BitmapLayer(props, {
+        //       data: undefined,
+        //       image: props.data,
+        //       bounds: [west, south, east, north],
+        //     });
+        //   },
+        // }),
       ];
     }
 
     return [];
-  }, [selectedRaster]);
+  }, [rasterImageUrl, selectedRaster]);
 
   const onClickMap = useCallback(
     (e: MapMouseEvent) => {
